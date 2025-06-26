7bc0bed66562ff2d0718af7e36cef721669278a0
kkkkk-npc <kkkkknpc@proton.me>
2025-06-26 14:07:30 +0800

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./info-sdk-core.cjs.production.min.js')
-} else {
-  module.exports = require('./info-sdk-core.cjs.development.js')
-}
